0b72446390eb058b5a278d6c3f0e25e2ef732dd8
m1sha <m1sha@live.ru>
2022-05-10 19:11:33 +0700

    v4

--- js/getpalette.js
+++ js/getpalette.js
@@ -1,31 +1,31 @@
-const
-  chooseFileBtn = document.getElementById('chooseFileBtn')
-const fileInput = document.getElementById('file')
-const imageView = document.getElementById('img')
-const imageView2 = document.getElementById('img2')
-const canvas = document.getElementById('canvas')
-const table = document.getElementById('table')
-const spinner = document.getElementById('spinner')
-const content = document.getElementById('content')
-const customRange = document.getElementById('customRange')
-const customRangePanel = document.getElementById('customRangePanel')
-const colorCounter = document.getElementById('colorCounter')
+const chooseFileBtn = byId('chooseFileBtn')
+const fileInput = byId('file')
+const imageView = byId('img')
+const imageView2 = byId('img2')
+const canvas = byId('canvas')
+const table = byId('table')
+const spinner = byId('spinner')
+const content = byId('content')
+const customRange = byId('customRange')
+const colorCounter = byId('colorCounter')
+const valueCounter = byId('value')
 const getPalette = eval('demo.getPalette')
 const whiteColor = eval('demo.whiteColor()')
 
-imageView.style.opacity = '0'
-spinner.style.display = 'none'
-content.style.opacity = '0'
-customRangePanel.style.opacity = '0'
-// progressBarLine.style.width = '0%'
+showLoading(false)
+showContent(false)
+
+valueCounter.textContent = customRange.value
+
 chooseFileBtn.onclick = function () { fileInput.click() }
 
 fileInput.onchange = function () {
   if (!fileInput.files) return
   const reader = new FileReader()
-  spinner.style.display = 'flex'
-  content.style.opacity = '0'
-  reader.readAsDataURL(fileInput.files[0])
+  showLoading(true)
+  const file = fileInput.files[0]
+  if (!file) return
+  reader.readAsDataURL(file)
   reader.onload = function () {
     imageView.style.opacity = '1'
     imageView.src = reader.result
@@ -34,31 +34,48 @@ fileInput.onchange = function () {
       imageView2.src = result.image
       drawPalette(result.list)
     }
+    imageView.onerror = function () {
+      alert("Error: image hasn't loaded")
+    }
+  }
+  reader.onerror = function () {
+    alert("Error: file hasn't loaded")
   }
 }
 
 customRange.onchange = function () {
+  valueCounter.textContent = customRange.value
   const result = getPalette(imageView, canvas, customRange.value)
   imageView2.src = result.image
   drawPalette(result.list)
 }
 
 function drawPalette (data) {
+  showLoading(true)
   table.innerHTML = ''
 
   for (let i = 0; i < data.length; i++) {
     const color = data[i]
-
     const div = document.createElement('div')
     div.innerHTML = color.toString() // + '<br />' + color.toHSV().toString()
     div.style.backgroundColor = 'rgba(' + color.r + ',' + color.g + ',' + color.b + ',' + color.a + ')'
-
-    div.style.color = color.getContrastRatio(whiteColor, true) > 5 ? '#fff' : '#000'
-
+    div.style.color = color.getContrastRatio(whiteColor, true) > 4 ? '#fff' : '#000'
     table.appendChild(div)
   }
   colorCounter.textContent = data.length
-  spinner.style.display = 'none'
-  content.style.opacity = '1'
-  customRangePanel.style.opacity = '1'
+
+  showLoading(false)
+  showContent(true)
+}
+
+function showContent (value) {
+  content.style.opacity = value ? '1' : '0'
+}
+
+function showLoading (value) {
+  spinner.style.display = value ? 'flex' : 'none'
+}
+
+function byId (id) {
+  return document.getElementById(id)
 }
